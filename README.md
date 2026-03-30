9c2a943c87296b5b3217fb26a87642ba29800ced
ronan.virmani <ronan.virmani@ufl.edu>
2026-03-30 19:44:59 -0400

    added graph functionality

--- README.md
+++ README.md
@@ -6,11 +6,19 @@ Ronan Virmani
 ## Requirements
 
 - Python 3
+- matplotlib (for graphing)
 
 ## How to Run
 
 ```bash
 python3 src/highest_value_lcs.py < data/input/example1.in
+```
+
+## Generate Runtime Graph
+
+```bash
+pip install matplotlib
+python3 src/graph.py
 ```
 
 ## Input Format
@@ -31,3 +39,10 @@ B
 <max_value>
 <subsequence>
 ```
+## Questions
+
+# 1. Runtime Analysis
+
+![Runtime Graph](runtime_graph.png)
+
+# 2. Recurrence Equation
---
+++ src/graph.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+def graph_runtimes(filepath):
+    names, sizes, times = [], [], []
+
+    with open(filepath) as f:
+        for line in f:
+            name, size, time = line.split()
+            names.append(name)
+            sizes.append(int(size))
+            times.append(float(time))
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(names, times)
+    plt.xlabel('Test File')
+    plt.ylabel('Runtime (ms)')
+    plt.title('Highest Value LCS Runtime')
+    plt.savefig('runtime_graph.png')
+
+if __name__ == "__main__":
+    graph_runtimes('data/runtimes.txt')
